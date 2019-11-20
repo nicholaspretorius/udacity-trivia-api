469ad5b10d916ef6ce5707a9d8fb6e4767ddbd63
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-11-20 16:07:42 +0200

    fix: Correct category type reference

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -115,7 +115,12 @@ class QuizView extends Component {
                 key={id}
                 value={id}
                 className="play-category"
-                onClick={() => this.selectCategory({ type: this.state.categories[id], id })}
+                onClick={() =>
+                  this.selectCategory({
+                    type: this.state.categories[id].type,
+                    id: this.state.categories[id].id
+                  })
+                }
               >
                 {this.state.categories[id].type}
               </div>
