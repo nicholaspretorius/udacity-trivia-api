611d3916a588f79807a8d2ef8d418415694c781d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-11-20 12:04:23 +0200

    feat: Add error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -180,6 +180,14 @@ def create_app(test_config=None):
             "message": "resource not found"
         }), 404
 
+    @app.errorhandler(422)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
     @app.errorhandler(500)
     def not_found(error):
         return jsonify({
